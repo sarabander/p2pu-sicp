5887c1c8ffdb5fb3ba5b0274d6d4907fb724183f
Andres Raba <sarabanderest@gmail.com>
2014-06-01 22:42:46 +0300

    set value to a connector, draw an environment

---
+++ 3.3/3.36.scm
@@ -0,0 +1,66 @@
+
+;; Dependencies
+
+(define (make-connector)
+  (let ((value #f) 
+        (informant #f) 
+        (constraints '()))
+    (define (set-my-value newval setter)
+      (cond ((not (has-value? me))
+             (set! value newval)
+             (set! informant setter)
+             (for-each-except setter inform-about-value constraints))
+            ((not (= value newval))
+             (error "Contradiction" (list value newval)))
+            (else 'ignored)))
+    (define (forget-my-value retractor)
+      (if (eq? retractor informant)
+          (begin (set! informant #f)
+                 (for-each-except 
+                  retractor
+                  inform-about-no-value
+                  constraints))
+          'ignored))
+    (define (connect new-constraint)
+      (if (not (memq new-constraint constraints))
+          (set! constraints (cons new-constraint constraints)))
+      (if (has-value? me)
+          (inform-about-value new-constraint))
+      'done)
+    (define (me request)
+      (cond ((eq? request 'has-value?) (if informant #t #f))
+            ((eq? request 'value) value)
+            ((eq? request 'set-value!) set-my-value)
+            ((eq? request 'forget) forget-my-value)
+            ((eq? request 'connect) connect)
+            (else (error "Unknown operation: CONNECTOR" request))))
+    me))
+
+(define (for-each-except exception procedure list)
+  (define (loop items)
+    (cond ((null? items) 'done)
+          ((eq? (car items) exception) 
+           (loop (cdr items)))
+          (else (procedure (car items))
+                (loop (cdr items)))))
+  (loop list))
+
+(define (has-value? connector)
+  (connector 'has-value?))
+(define (get-value connector)
+  (connector 'value))
+(define (set-value! connector new-value informant)
+  ((connector 'set-value!) new-value informant))
+(define (inform-about-value constraint)
+  (constraint 'I-have-a-value))
+(define (connect connector new-constraint)
+  ((connector 'connect) new-constraint))
+
+;; Exercise starts here
+
+(define a (make-connector))
+(define b (make-connector))
+(set-value! a 10 'user)
+
+; The resulting environment E10 is depicted in the file 3.36.png.
+; The diagram is done with EnvDraw utility in Stk Scheme.
