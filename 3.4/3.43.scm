d245a4ba3d873de9ca6b50e701bb472c7bc95352
Andres Raba <sarabanderest@gmail.com>
2014-06-03 23:00:29 +0300

    add timing diagrams

--- 3.4/3.43.scm
+++ 3.4/3.43.scm
@@ -7,9 +7,10 @@
 ;; as both withdrawal and deposit succeed or fail together. This way
 ;; the system is closed -- at any moment the total sum is distributed
 ;; among accounts and temporary variables (like difference). The amount
-;; taken away from one place will be put back in another place. 
+;; taken away from one place will be put back in another place. See
+;; the first diagram in 3.43.pdf.
 
 ;; If withdrawal and deposit are not serialized, we are not guaranteed
 ;; to possess the latest information about any account's balance. It could
-;; change between accessing the balance and setting new balance, 
-;; as shown in figure 3.29. So we leak money in or out from the system.
+;; change between accessing the balance and setting new balance, as shown
+;; on the second diagram in 3.43.pdf. We leak or gain money in the system.
